e295a3f3db7c99fb6d4b272113fd2ae7d137e053
Hashindiriye <diriye96@gmail.com>
2024-09-15 22:07:17 +0200

    Ny Arv och Polymorphism

---
+++ Arv 2 .cs
@@ -0,0 +1,48 @@
+﻿using System; // Behövs för Console.WriteLine()
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Skapa en ny Rawbike
+        Rawbike myRawbike = new Rawbike();
+
+        // Anropa metoden scream()
+        myRawbike.scream(); // Utskrift: "This Rawbike is silent while riding!"
+
+        // Anropa metoden speed()
+        myRawbike.speed(); // Utskrift: "This Rawbike is incredibly fast!"
+    }
+}
+
+// HUVUDKLASSEN
+public class Bike
+{
+    public string model;
+    public int year;
+
+    // METOD AV KLASSEN
+    public virtual void scream()
+    {
+        Console.WriteLine("This bike makes a sound");
+    }
+}
+
+// Underklass Rawbike som ärver från Bike
+public class Rawbike : Bike
+{
+    public int batteryCapacity;
+
+    // Override för att modifiera ljudet
+    public override void scream()
+    {
+        Console.WriteLine("This Rawbike is silent while riding!");
+    }
+
+    // Ny metod för hastighet
+    public void speed()
+    {
+        Console.WriteLine("This Rawbike is incredibly fast!");
+    }
+}
+
---
+++ Polymorfism 2.cs
@@ -0,0 +1,103 @@
+﻿
+namespace Alla_4_oop_priciper_inlämingsuppgift
+{
+
+    namespace ChipExamples
+    {
+        public class Chips
+        {
+            public string Name;
+            public string Description;
+
+            public virtual void Start()
+            {
+                Console.WriteLine("These chips are delicious");
+            }
+        }
+
+        public class Estrella : Chips
+        {
+            public override void Start()
+            {
+                Console.WriteLine("De här Estrella chipsen är goda");
+            }
+        }
+
+        public class Doritos : Chips
+        {
+            public override void Start()
+            {
+                Console.WriteLine("Dessa Doritos är crunchy");
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                // Skapa en instans av basklassen Chips och anropa metoden Start
+                Chips myChips = new Chips();
+                myChips.Start();
+
+                // Skapa en instans av Estrella och anropa den överskrivna Start-metoden
+                Estrella estrellaChip = new Estrella();
+                estrellaChip.Start();
+
+                // Skapa en instans av Doritos och anropa den överskrivna Start-metoden
+                Doritos doritosChip = new Doritos();
+                doritosChip.Start();
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
